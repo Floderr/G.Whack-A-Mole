c616d9cb090ec0e9a0310fb5b99dd7296b50a0fd
Flode <126754789+Flodez@users.noreply.github.com>
2024-01-14 03:06:53 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let score = 0;
+
+function createMole() {
+  const mole = document.createElement('div');
+  mole.classList.add('mole');
+  mole.style.left = `${Math.random() * (window.innerWidth - 50)}px`;
+  mole.style.top = `${Math.random() * (window.innerHeight - 50)}px`;
+  mole.addEventListener('click', whackMole);
+  document.body.appendChild(mole);
+
+  setTimeout(() => {
+    mole.remove();
+    createMole();
+  }, 2000);
+}
+
+function whackMole() {
+  score += 1;
+  document.getElementById('score').textContent = `${score}`;
+  this.remove();
+}
+
+createMole();
